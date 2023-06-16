18c6fd6831f072e21b9ae2437daf22950a0aee85
Jack Christian <ccontact@jackchristian.uk>
2023-06-16 15:21:36 +0100

    jetblue more blue sams amends

--- More_Blue/dynamic/richLoads/Summer_23_retail_CR05_300x250_RL1/mainRL1.js
+++ More_Blue/dynamic/richLoads/Summer_23_retail_CR05_300x250_RL1/mainRL1.js
@@ -130,9 +130,9 @@ function animate() {
   .to(['#h3', '#terms1', '#terms2'], 0.5, { autoAlpha: 1, ease: Power1.easeOut}, 'frame3+=1.5')
 
   .addLabel('frame4', "frame3+=1.5")
-  .to('#plane', 7, { x: "-=540", ease: Power1.easeInOut}, 'frame4')
+  .to('#plane', 7, { x: "-=500", ease: Power1.easeInOut}, 'frame4')
 
-  .addLabel('frame_END', "frame4+=5")
+  .addLabel('frame_END', "frame4+=4.75")
   .to('#endframeBg', 0.6 ,{ top: 0, ease: Back.easeOut.config(.3)}, 'frame_END')
   .to(['#terms1', '#terms2'], 0.5, { autoAlpha: 0, ease: Power1.easeOut}, 'frame_END')
    
